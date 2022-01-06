b37003921c49938adffd739918968b577593bf40
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2022-01-06 23:14:50 +0700

    add eslint ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,32 @@
-import './App.css'
-
-import { useState } from 'react'
-
 import logo from './logo.svg'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="flex flex-col h-screen justify-center items-center bg-gray-500">
+      <img
+        src={logo}
+        className="animate-spin"
+        alt="logo"
+        width="200"
+        height="200"
+      />
+      <h1 className="text-3xl font-bold underline cursor-pointer text-white hover:text-red-500">
+        Hello world!
+      </h1>
+      <div className="mt-10 border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+        <div className="animate-pulse flex space-x-4">
+          <div className="rounded-full bg-gray-700 h-10 w-10"></div>
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 bg-gray-700 rounded"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="h-2 bg-gray-700 rounded col-span-2"></div>
+                <div className="h-2 bg-gray-700 rounded col-span-1"></div>
+              </div>
+              <div className="h-2 bg-gray-700 rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
